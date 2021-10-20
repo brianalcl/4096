918efc5edac6491d05aff322073152f8102bac2c
brian <brianclemente@outlook.com>
2021-10-20 19:00:53 -0300

    Sistema de guardado de puntaje usando properties

--- src/data/Datos.java
+++ src/data/Datos.java
@@ -4,25 +4,39 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.Properties;
 
 import game.Juego;
 import main.Player;
 
 public class Datos {
 	private Juego miJuego;
+	private Properties configuration;
 	
 	public Datos(Juego juego) {
 		this.miJuego = juego;
+		cargarConfiguracion();
 	}
+	
+	public void cargarConfiguracion() {
+		try (InputStream input = new FileInputStream("./src/resources/configuration.properties")) {
+
+            configuration = new Properties();
+            configuration.load(input);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
 	
 	public void salirDelJuego() {
 		try {
-			FileOutputStream fileOutputStream
-			= new FileOutputStream("./score.dat");
-			ObjectOutputStream objectOutputStream 
-			= new ObjectOutputStream(fileOutputStream);
+			FileOutputStream fileOutputStream = new FileOutputStream(configuration.getProperty("file"));
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
 			objectOutputStream.writeObject(miJuego.getJugador());
 			objectOutputStream.flush();
 			objectOutputStream.close();
@@ -35,10 +49,8 @@ public class Datos {
 	
 	public void entrarAlJuego() {
 		try {
-			FileInputStream fileInputStream
-			= new FileInputStream("./score.dat");
-			ObjectInputStream objectInputStream
-			= new ObjectInputStream(fileInputStream);
+			FileInputStream fileInputStream = new FileInputStream(configuration.getProperty("file"));
+			ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
 			miJuego.setJugador((Player) objectInputStream.readObject());
 			miJuego.addBestScore(miJuego.getJugador().getBestScore());
 			objectInputStream.close();
